22f365c662d36203bb4f16167ae8e13624bdeb35
David <david.loh18.dl@gmail.com>
2023-05-26 20:05:23 +0200

    Version number

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,18 @@
           <nuxt-link to="/buildings">Gebäude</nuxt-link>
           <nuxt-link to="/vehicles">Fahrzeuge</nuxt-link>
         </nav>
+        <div class="version">
+          <div class="client">
+            <p class="version-label">Client</p>
+            <p class="version-number">Version: 1.0.0</p>
+            <p class="build-number">Build: 001 (Development)</p>
+          </div>
+          <div class="server">
+            <p class="version-label">Server</p>
+            <p class="version-number">Version: X.X.X</p>
+            <p class="build-number">Build: XXX</p>
+          </div>
+        </div>
       </aside>
       <main>
         <Nuxt />
@@ -37,7 +49,7 @@ header + * {
 }
 
 aside {
-  @apply fixed shadow-md h-full bg-slate-50 p-2 w-40;
+  @apply fixed shadow-md bg-slate-50 p-2 w-40 bottom-0 left-0 top-12 flex flex-col justify-between;
 }
 
 aside nav {
@@ -52,6 +64,18 @@ aside a {
 aside a.nuxt-link-exact-active {
   @apply bg-red-600 text-slate-50
   hover:bg-red-700;
+}
+
+aside .version {
+  @apply text-slate-300 text-xs;
+}
+
+aside .version .version-label {
+  @apply text-sm;
+}
+
+aside .version > div {
+  @apply mb-2;
 }
 
 main {
